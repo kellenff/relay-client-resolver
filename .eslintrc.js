caa40f4048e6d67a706a72c047143cc0ad4e1ad4
Kellen Frodelius-Fujimoto <kellen@kellenfujimoto.com>
2021-06-11 14:21:58 -0700

    Add code quality tools

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  env: {
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      arrowFunctions: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts"],
+        paths: ["./src"],
+      },
+    },
+  },
+};
